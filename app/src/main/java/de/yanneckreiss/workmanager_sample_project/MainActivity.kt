99e4c926ebb41c48a541f92a92409b31cfacda7b
Yanneck Reiß <100080106+YanneckReiss@users.noreply.github.com>
2023-03-14 11:30:34 +0100

    Update MainActivity.kt

--- app/src/main/java/de/yanneckreiss/workmanager_sample_project/MainActivity.kt
+++ app/src/main/java/de/yanneckreiss/workmanager_sample_project/MainActivity.kt
@@ -14,11 +14,11 @@ import de.yanneckreiss.workmanager_sample_project.ui.home.HomeScreen
 import de.yanneckreiss.workmanager_sample_project.ui.theme.WorkmanagerSampleProjectTheme
 
 class MainActivity : ComponentActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WorkmanagerSampleProjectTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -27,18 +27,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WorkmanagerSampleProjectTheme {
-        Greeting("Android")
     }
 }
